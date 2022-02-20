c1845e3c091e4ffa59026a4e154c94a5bb41c088
rodionovmax <rodionovmax90test@gmail.com>
2022-02-20 13:39:31 -0500

    lesson3

--- app/src/main/java/com/gb/weatherapp/model/City.kt
+++ app/src/main/java/com/gb/weatherapp/model/City.kt
@@ -1,7 +1,11 @@
 package com.gb.weatherapp.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class City(
     val city: String,
     val lat: Double,
     val lon: Double
-)
+) : Parcelable
--- app/src/main/java/com/gb/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/gb/weatherapp/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gb.weatherapp.R
 import com.gb.weatherapp.databinding.MainActivityBinding
+import com.gb.weatherapp.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
